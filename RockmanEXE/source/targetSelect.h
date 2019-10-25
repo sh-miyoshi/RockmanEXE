3dc825810b7b5faa4e1c2c4f8a14f6af5195480f
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-10-25 09:28:25 +0900

    add target info class

--- RockmanEXE/source/targetSelect.h
+++ RockmanEXE/source/targetSelect.h
@@ -1,9 +1,39 @@
 #pragma once
 
+#include <vector>
+#include <string>
 #include "fade.h"
 #include "selecter.h"
+#include "point.h"
 
 class TargetSelect {
+	enum {
+		eTRG_ÉÅÉbÉgÅ[Éãx1,
+
+		eTRG_MAX
+	};
+
+	class EnemyInfo{
+	public:
+		int id;// TODO(use enum)
+		CPoint<int> viewPos;
+		CPoint<int> battleInitPos;
+
+		EnemyInfo(int id, CPoint<int> viewPos, CPoint<int> battleInitPos)
+			:id(id), viewPos(viewPos), battleInitPos(battleInitPos){}
+		~EnemyInfo(){}
+	};
+
+	class TargetInfo{
+	public:
+		std::string name;
+		std::vector<EnemyInfo> info;
+
+		TargetInfo(std::string name, std::vector<EnemyInfo> info)
+			:name(name), info(info){}
+		~TargetInfo(){}
+	};
+
 	Selecter selecter;
 public:
 	enum RtnCode {
